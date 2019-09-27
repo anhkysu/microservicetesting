9fb15a74fc4f5ce00814216920453910c9d429cd
TS-902 <TS-902@5IVNJP.LOCAL>
2019-09-27 19:19:08 +0700

    hazaa

--- MongoTest.js
+++ MongoTest.js
@@ -1,13 +1,12 @@
 const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://akysu:<trinhkhanh>@aks01-ghsgg.gcp.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
 
-// replace the uri string with your connection string.
-const uri = "mongodb+srv://akysu:<trinhkhanh>@aks01-ghsgg.gcp.mongodb.net/test?retryWrites=true&w=majority"
-MongoClient.connect(uri, function(err, client) {
-   if(err) {
-        console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
-   }
-   console.log('Connected...');
-   const collection = client.db("test").collection("devices");
-   // perform actions on the collection object
-   client.close();
-});
+client.connect((err, client) => {
+    if (err) console.log(err)
+    else {
+        const collection = client.db("test").collection("devices");
+        // perform actions on the collection object
+        client.close();
+    }
+   });
